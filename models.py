a8015354825138bf5eed99b621fa54d591377614
Sala 2 <sala2@FI-LabSistemas.ucatolica.loc>
2025-12-05 08:06:31 -0500

    estoy empezando con models ya puse jugador, HAZ TU UN COMMIT

--- models.py
+++ models.py
@@ -1,8 +1,28 @@
-class Jugador():
-    pass
+from typing import Optional, List
+from datetime import date, datetime
+from sqlmodel import SQLModel, Field, Relationship
+
+class JugadorDB(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    numero: int = Field(..., ge=1, le=99, description="Número de camiseta único 1-99")
+    nacionalidad: Optional[str] = None
+    fecha_nacimiento: Optional[date] = None
+    altura_cm: Optional[float] = None
+    peso_kg: Optional[float] = None
+    posicion: Optional[str] = None
+    pie_habil: Optional[str] = None
+    estado: str = Field(default="ACTIVO")
+    foto_url: Optional[str] = None
+
+    estadisticas: List["EstadisticaDB"] = Relationship(back_populates="jugador")
+pass
+
+
 
 
 class Estadistica():
+
     pass
 
 
